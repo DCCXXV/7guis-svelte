a09b1196f06a02f8a56e1fb9428dd4bd4678214a
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2025-04-22 00:20:53 +0200

    feat: counter!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
-
-<h1>7GUIs: A GUI Programming Benchmark</h1>
-<h3>... done in Svelte 5 ✨</h3>
-<p>
-    Original <a href="https://eugenkiss.github.io/7guis/">website</a>. 
-</p>
-<p>
-    Collection of different graphical user interfaces I built following the 7 GUIs Programming Benchmark to learn svelte and sveltekit. Styling is done with classless picocss for simplicity as it is not the main focus of this practice.
-</p>
-<div id="img-container">
-    <img src="favicon.png" alt="Svelte's logo" style="width: 30vw">
-</div>
-
+<section>
+    <h1>7GUIs: A GUI Programming Benchmark</h1>
+    <h3>... done in Svelte 5 ✨</h3>
+    <p>
+        Original <a href="https://eugenkiss.github.io/7guis/">website</a>. 
+    </p>
+    <p>
+        Collection of different graphical user interfaces I built following the 7 GUIs Programming Benchmark to learn svelte and sveltekit. Styling is done with classless picocss for simplicity as it is not the main focus of this practice.
+    </p>
+    <div id="img-container">
+        <img src="favicon.png" alt="Svelte's logo" style="width: 30vw">
+    </div>
+</section>
 <style>
     #img-container {
         display: flex;
--- src/routes/counter/+page.svelte
+++ src/routes/counter/+page.svelte
@@ -1,2 +1,31 @@
-<h1>#1 Counter</h1>
-<p>TO DO</p>
+<script lang="ts">
+    let count = $state(0);
+
+    function increment() {
+        count++;
+    }
+</script>
+
+<section>
+    <div id="content">
+        <span id="number">{count}</span>
+        <button onclick={increment}>Count</button>
+    </div>
+</section>
+
+<style>
+    section {
+        display: flex;
+        height: 70vh;
+    }
+
+    #content {
+        display: grid;
+        margin: auto;
+        justify-items: center;
+    }
+    #number {
+        font-size: 5rem;
+        font-weight: bold;
+    }
+</style>
